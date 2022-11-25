9ec779fea98ba00349099f861bfdd9ae2a70fa74
inekeyuliam <inekemargareta20@gmail.com>
2022-11-25 23:24:28 +0700

    Initial Commit

---
+++ MessengerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessengerApp
+//
+//  Created by Ineke Yulia Margareta on 25/11/22.
+//  Copyright © 2022 Ineke Yulia Margareta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
